45eeb078eae1eeb72b0a4af8ea1fccda3bfd06f3
Bruno Jan <brunojan@boyu66.net>
2020-09-02 16:52:28 +0800

    Updates

---
+++ component---src-pages-404-tsx-2c58f41fde4ecaf283b9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{"i6+/":function(e,a,t){"use strict";t.r(a);var n=t("Hesl"),l=t("HHUu"),c=t("q1tI"),i=t.n(c);a.default=function(e){return void 0===e.data?i.a.createElement(n.a,null):i.a.createElement(n.a,{title:"失聯！"},i.a.createElement("div",{className:l.gPl20Lg}))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-2c58f41fde4ecaf283b9.js.map
--- component---src-pages-friends-tsx-676005fb0e9efe856c39.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{XpLn:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),c=t.n(a),s=t("HHUu"),i=t("Hesl");n.default=function(e){return c.a.createElement(i.a,{title:"友站連結"},c.a.createElement("div",{className:s.gPl20Lg}))}}}]);
-//# sourceMappingURL=component---src-pages-friends-tsx-676005fb0e9efe856c39.js.map
