c84c1d89f32dca7b34d8b6874e146a2a4045fc8e
Bruno Jan <brunojan@boyu66.net>
2020-09-04 17:56:11 +0800

    Updates

--- component---src-pages-404-tsx-2529a91310d634ad6410.js
+++ component---src-pages-404-tsx-2529a91310d634ad6410.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"i6+/":function(e,a,t){"use strict";t.r(a);var n=t("Hesl"),l=t("HHUu"),c=t("q1tI"),i=t.n(c);a.default=function(e){return void 0===e.data?i.a.createElement(n.a,null):i.a.createElement(n.a,{title:"失聯！"},i.a.createElement("div",{className:l.gPl20Lg}))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-aa5d11d4ec2063f7c8c5.js.map
+//# sourceMappingURL=component---src-pages-404-tsx-2529a91310d634ad6410.js.map
