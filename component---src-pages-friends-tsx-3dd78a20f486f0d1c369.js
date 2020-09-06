035966504bd8da4f3f86f1000d6f9fd54e6bb733
Bruno Jan <brunojan@boyu66.net>
2020-09-06 15:21:43 +0800

    Updates

--- component---src-pages-friends-tsx-3dd78a20f486f0d1c369.js
+++ component---src-pages-friends-tsx-3dd78a20f486f0d1c369.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[10],{XpLn:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),c=t.n(a),s=t("HHUu"),i=t("Hesl");n.default=function(e){return c.a.createElement(i.a,{title:"友站連結"},c.a.createElement("div",{className:s.gPl20Lg}))}}}]);
-//# sourceMappingURL=component---src-pages-friends-tsx-b9218dc1507f3e5241cd.js.map
+//# sourceMappingURL=component---src-pages-friends-tsx-3dd78a20f486f0d1c369.js.map
