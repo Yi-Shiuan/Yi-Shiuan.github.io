25e2d093dd0e040bcd8de7793161f6b80f196ed8
Bruno Jan <brunojan@boyu66.net>
2021-01-08 13:50:10 +0800

    Updates

---
+++ component---src-pages-404-tsx-baa8ba9d7a98321408f4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{"i6+/":function(e,a,t){"use strict";t.r(a);var n=t("Hesl"),l=t("HHUu"),c=t("q1tI"),i=t.n(c);a.default=function(e){return void 0===e.data?i.a.createElement(n.a,null):i.a.createElement(n.a,{title:"失聯！"},i.a.createElement("div",{className:l.gPl20Lg}))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-baa8ba9d7a98321408f4.js.map
