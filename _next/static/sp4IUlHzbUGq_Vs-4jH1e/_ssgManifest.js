7e736a9125b206514ab72c3bda0dc3086871e835
Bruno Jan <brunojan@gmail.com>
2022-10-20 16:36:56 +0800

    Updates

--- _next/static/sp4IUlHzbUGq_Vs-4jH1e/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002F404","\u002F500","\u002Fabout","\u002Fposts\u002F[...post]","\u002Fresume","\u002Ftags\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
