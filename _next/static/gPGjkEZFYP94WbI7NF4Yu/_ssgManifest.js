2785743319a5f78a5a4394689e4ac550ba0eb505
brunojan <amour99520@gmail.com>
2022-12-04 15:46:28 +0800

    Updates

---
+++ _next/static/gPGjkEZFYP94WbI7NF4Yu/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F404","\u002F500","\u002F[page]","\u002Fabout","\u002Fposts\u002F[...post]","\u002Fresume\u002F[lang]","\u002Ftags\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
